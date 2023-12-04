684031bd86aaf0920fd41ac67ef47239d3865867
arshad <mdarshadwarshi72@gmail.com>
2023-12-04 10:31:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import JobCard from "./components/JobCard";
+import LandingPage from "./components/LandingPage";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      {/* <JobCard /> */}
+    </>
+  );
+}
+
+export default App;
